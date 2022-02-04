d8bc84614cb71acf4f67d94a378db314a3d0352f
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-02-04 16:32:14 +0200

    add task 2 solution

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const createIngredientsList = ingredients.map(ingredient => {
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.textContent = ingredient;
+  ingredientsEl.classList.add('item');
+  return ingredientsEl;
+});
+const ingredientsContainer = document.querySelector('#ingredients');
+ingredientsContainer.append(...createIngredientsList);
