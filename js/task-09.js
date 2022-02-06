9ea2d7700771119779995ba3b4389024197ff773
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-02-06 13:32:50 +0200

    add task 9 solution

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const colorValueEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+const onBtnClick = event => {  
+  bodyEl.style = `background-color: ${getRandomHexColor()};`
+  colorValueEl.textContent = getRandomHexColor();
+}
+btnEl.addEventListener('click', onBtnClick);
