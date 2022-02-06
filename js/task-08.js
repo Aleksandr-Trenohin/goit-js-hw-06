74af50399b5a2649bee45da0d233290ef72a85c5
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-02-06 09:56:33 +0200

    add task 8 solution

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginFormEl = document.querySelector('.login-form');
 
+loginFormEl.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = loginFormEl.email.value;
+  const password = loginFormEl.password.value;
+  if (email === '' || password === '') {
+    alert('Все поля должны быть заполнены!');
+  } else {
+    const formData = {
+      email,
+      password,
+      };
+      console.log(formData);
+  }
+  loginFormEl.reset(); 
+}
